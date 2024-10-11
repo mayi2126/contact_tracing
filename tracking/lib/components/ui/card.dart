b63f7b4f2db115122c131054d39c7af6909cd654
fideleChester <ekloufidele82@gmail.com>
2024-10-11 11:58:10 +0000

    clic effect on card

--- tracking/lib/components/ui/card.dart
+++ tracking/lib/components/ui/card.dart
@@ -1,35 +1,60 @@
 part of '../../core/cores.dart';
 
-class CardWidget extends StatelessWidget {
-  const CardWidget({super.key, required this.icon, required this.title, this.onTap,});
+class CardWidget extends StatefulWidget {
+  const CardWidget({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.onTap,
+  });
   final IconData icon;
   final String title;
   final Function()? onTap;
 
+  @override
+  State<CardWidget> createState() => _CardWidgetState();
+}
 
-
+class _CardWidgetState extends State<CardWidget> {
+   Color _containerColor = Palette.card;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTapDown: (_) {
+        setState(() {
+          _containerColor = Palette.primary; // Change la couleur sur le clic
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _containerColor = Palette.card; // Rétablit la couleur d'origine
+        });
+      },
+      onTap: widget.onTap,
       child: Container(
         height: getHeight(120),
         width: getWidth(50),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Palette.card,
-          ),
-        child:  Column(
+          borderRadius: BorderRadius.circular(10),
+          color: _containerColor,
+        ),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
+          children: [
             10.verticalSpace,
             CircleAvatar(
               radius: 10,
               backgroundColor: Colors.transparent,
-              child:  Icon(icon,),
+              child: Icon(
+                widget.icon,
+              ),
             ),
             10.verticalSpace,
-            Text(title,textAlign: TextAlign.center,)
+            Text(
+              widget.title,
+              textAlign: TextAlign.center,
+            )
           ],
         ),
       ),
